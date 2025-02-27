36a88918d83241bc356be31c26f03da04089d5f3
ColinLug <114562993+ColinLug@users.noreply.github.com>
2025-02-27 11:48:15 +0100

    Add files via upload
    Add test files

---
+++ test1/test.py
@@ -0,0 +1,10 @@
+from PyPDF2 import PdfReader
+from scihub import SciHub
+
+sh = SciHub()
+result = sh.download("https://dl.acm.org/doi/abs/10.1145/2858036.2858132", path="test5.pdf")
+reader = PdfReader("test5.pdf")
+number_of_pages = len(reader.pages)
+page = reader.pages[0]
+text = page.extract_text()
+print(text)
